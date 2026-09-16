94d282810c287c8afd576ed3216adab36ef6601e
Adarsh Maurya <adarshmaurya2303@gmail.com>
2026-09-16 14:46:05 +0530

    add example of list operations including length, append, indexing, slicing, and iteration in Python

---
+++ data_types.py
@@ -0,0 +1,20 @@
+# List
+my_list = [1, 2, 3, 4, 5]
+print(my_list, type(my_list))
+
+# length of list
+print(len(my_list))
+
+# append
+my_list.append(6)
+
+# indexing
+print(my_list[0])  # first element
+print(my_list[-1])  # last element
+
+# slicing(list[start:end])
+# print(my_list[1:4])
+print(my_list[-3:])
+
+for m in my_list:
+    print(m)
