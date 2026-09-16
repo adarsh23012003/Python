7b33b24e328e58c7b540908c51c5a81e4235bcc9
Adarsh Maurya <adarshmaurya2303@gmail.com>
2026-09-16 13:55:41 +0530

    add example of using the `range()` function in Python

---
+++ loops/range.py
@@ -0,0 +1,5 @@
+# This is a simple example of using the `range()` function in Python. The `range()` function generates a sequence of numbers, which can be used in loops or for other purposes. In this case, `range(10)` generates numbers from 0 to 9.
+# range(start, stop, step) can also be used to generate a sequence of numbers with a specified start, stop, and step value.
+# default values for start and step are 0 and 1, respectively.
+
+print(range(10))
